4207bf0f629086409d1fab308586a734f95894b4
Agrim Bansal <agrimx2@gmail.com>
2020-12-29 20:17:13 +0530

    Initial commit

---
+++ frontend/index.js
@@ -0,0 +1,24 @@
+const url = "http:localhost:5000/currentWheather"
+
+const request = {
+    method : 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+      }
+}
+
+var place = ""
+
+getWheather = async () => {
+    
+    data = {place : place}
+    request.body = JSON.stringify(data)
+
+    const result = await fetch(url, request)
+    const wheather = await result.json()
+
+    console.log(wheather)
+}
+
+
+document.querySelector('#button').addEventListener('click', getWheather)
